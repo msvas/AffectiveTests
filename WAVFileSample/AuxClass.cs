7d22893dce13284f3a0f9f21ca420ec5f2312332
Marcelo Vasques <msouvas@gmail.com>
2014-06-24 10:52:33 +0100

    Printing WAV files

---
+++ WAVFileSample/AuxClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WAVFileSample
+{
+    public class AuxClass
+    {
+        public double[] leftAudio;
+        public double[] rightAudio;
+        string soundName = "wavTest.wav";
+        Program program = new Program();
+
+        public AuxClass()
+        {
+            program.openWav(soundName, out leftAudio, out rightAudio);
+        }
+    }
+}
