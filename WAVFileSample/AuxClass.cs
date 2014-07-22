9b8ba20054f6bc95dbe20b5ac522da032fbcb2fe
Khin Baptista <khin.baptista@gmail.com>
2014-07-22 11:30:29 +0100

    Stopwatches and more prints for testing

--- WAVFileSample/AuxClass.cs
+++ WAVFileSample/AuxClass.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using ILNumerics; 
+using ILNumerics;
+using System.Diagnostics;
 
 namespace NDtw
 {
@@ -19,6 +20,8 @@ namespace NDtw
         Correlation crossCorr;
         public int offset;
         RealTime recorder = new RealTime();
+        Stopwatch stopwatchProcess = new Stopwatch();
+        Stopwatch stopwatchRecord = new Stopwatch();
 
         public AuxClass()
         {
@@ -26,11 +29,14 @@ namespace NDtw
             {
                 if (recorder.checkMic())
                 {
-                    Console.ReadKey();
+                    Console.WriteLine("Press any key to start recording");
+                    Console.ReadKey(); Console.WriteLine("Recording..."); stopwatchRecord.Start();
                     recorder.startRecording();
                     Console.ReadKey();
-                    recorder.stopRecording();
+                    recorder.stopRecording(); stopwatchRecord.Stop();
+                    Console.WriteLine("Sound recorded. Processing...");
 
+                    stopwatchProcess.Start();
                     program.openWav(soundName, out leftAudio, out rightAudio);
                     program.openWav("Test0001.wav", out leftCompared, out rightCompared);
 
@@ -74,7 +80,11 @@ namespace NDtw
 
                     crossCorr = new Correlation(result, leftAudio, leftCompared, out offset, out value);
 
-                    Console.WriteLine(value);
+                    stopwatchProcess.Stop();
+                    Console.WriteLine("Value: " + value);
+                    Console.WriteLine("Time elapsed recording: " + stopwatchRecord.ElapsedMilliseconds + " ms");
+                    Console.WriteLine("Time elapsed processing: " + stopwatchProcess.ElapsedMilliseconds + " ms");
+                    Console.WriteLine("Press spacebar to close, anything else to start over \r\n");
                 }
             }
             while (Console.ReadKey().Key != ConsoleKey.Spacebar);
